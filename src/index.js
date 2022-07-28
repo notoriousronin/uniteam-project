dd04f961a9d97e4f56cf4bd527d6ca85a1084c23
etelight <etelight@gmail.com>
2022-07-28 10:39:22 +0300

    add scroll to top

--- src/index.js
+++ src/index.js
@@ -56,3 +56,30 @@
 //     document.body.scrollTop = 0; // For Safari
 //     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 // }
+
+const scrollToTopBtn = document.getElementById('go-to-top-btn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    scrollToTopBtn.style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
+};
